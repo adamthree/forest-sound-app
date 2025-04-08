f755b32faaf7f98ce9c8765d6896be9e333f7e54
adam <adam@example.com>
2025-04-09 05:19:26 +0800

    Add SPA routing support for GitHub Pages

--- publicpath.js
+++ publicpath.js
@@ -1,4 +1,2 @@
 // Set the correct public path for GitHub Pages
-__webpack_public_path__ = window.location.pathname.includes('forest-sound-app') 
-  ? '/forest-sound-app/' 
-  : '/'; 
+__webpack_public_path__ = window.baseUrl || '/'; 
